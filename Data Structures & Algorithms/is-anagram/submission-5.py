19c698dc79bfa4ec4a04622d46a9bf752f1c6126
andrewthenedi <44109741+andrewthenedi@users.noreply.github.com>
2026-06-03 15:59:46 +0800

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # T: O(N + M) | S: O(1)
+        # N = Number of characters in s
+        # M = Number of characters in t
+        if len(s) != len(t):
+            return False
+        counter_s = Counter(s)
+        for c in t:
+            if c not in counter_s:
+                return False
+            counter_s[c] -= 1
+            if not counter_s[c]:
+                del counter_s[c]
+        return not counter_s
