889f2ddecf0dced2c461454a3b3e914994864536
andrewthenedi <44109741+andrewthenedi@users.noreply.github.com>
2026-06-06 16:42:55 +0800

    Add: permutation-string - submission-10

---
+++ Data Structures & Algorithms/permutation-string/submission-10.py
@@ -0,0 +1,23 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        # T: O(N + M) | S: O(1)
+        # N = Length of s1
+        # M = Length of s2
+        n_s1, n_s2 = len(s1), len(s2)
+        if n_s1 > n_s2:
+            return False
+        count_s1 = [0] * 26
+        count_s2 = [0] * 26
+        for i in range(n_s1):
+            count_s1[ord(s1[i]) - ord('a')] += 1
+            count_s2[ord(s2[i]) - ord('a')] += 1
+        if count_s1 == count_s2:
+            return True
+        l = 0
+        for r in range(n_s1, n_s2):
+            count_s2[ord(s2[l]) - ord('a')] -= 1
+            l += 1
+            count_s2[ord(s2[r]) - ord('a')] += 1
+            if count_s1 == count_s2:
+                return True
+        return False
