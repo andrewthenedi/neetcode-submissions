6618bb52ccbf0c151dea08b7be6e24130fb2b07b
andrewthenedi <44109741+andrewthenedi@users.noreply.github.com>
2026-06-11 15:31:04 +0800

    Add: daily-temperatures - submission-7

---
+++ Data Structures & Algorithms/daily-temperatures/submission-7.py
@@ -0,0 +1,13 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        # T: O(N) | S: O(N)
+        # N = Size of temperatures
+        result = [0] * len(temperatures)
+        colder = []
+        for i, temp in enumerate(temperatures):
+            while colder and colder[-1][0] < temp:
+                j = colder[-1][1]
+                result[j] = i - j
+                colder.pop()
+            colder.append([temp, i])
+        return result
