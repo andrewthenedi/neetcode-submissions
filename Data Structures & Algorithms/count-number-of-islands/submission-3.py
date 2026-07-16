9b6d8945a78f28fb79b2a61f68baaa59bc6b40d9
andrewthenedi <44109741+andrewthenedi@users.noreply.github.com>
2026-07-16 14:38:28 +0800

    Add: count-number-of-islands - submission-3

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-3.py
@@ -0,0 +1,27 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        # T: O(M * N) | S: O(M * N)
+        # M = Size of grid rows
+        # N = Size of grid columns
+        visited = set()
+        num_islands = 0
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        m, n = len(grid), len(grid[0])
+        for r in range(m):
+            for c in range(n):
+                if grid[r][c] == "0" or (r, c) in visited:
+                    continue
+                stack = [(r, c)]
+                visited.add((r, c))
+                while stack:
+                    r_island, c_island = stack.pop()
+                    for r_direction, c_direction in directions:
+                        r_next, c_next = r_island + r_direction, c_island + c_direction
+                        is_within_bounds = (0 <= r_next < m) and (0 <= c_next < n)
+                        if is_within_bounds \
+                            and grid[r_next][c_next] == "1" \
+                            and (r_next, c_next) not in visited:
+                            stack.append((r_next, c_next))
+                            visited.add((r_next, c_next))
+                num_islands += 1
+        return num_islands
